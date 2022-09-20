e42ce6e4d3b59a5be6ee21869813d7d94af30043
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 14:14:28 +0200

    Seite 7, Aufgabe 6

--- DataImport/DataImport/Program.cs
+++ DataImport/DataImport/Program.cs
@@ -12,33 +12,40 @@ namespace DataImport
         static void Main(string[] args)
         {
             //004 Aufgaben Wiederholung Tag 1
-            //Seite 2, Aufgabe 2 & 3
+            //Seite 7, Aufgabe 2 & 3
             //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
             //fi.CopyTo("C:/001Projects/meine_datei.csv");
             //Console.ReadKey();
 
-            //Seite 2, Aufgabe 4
+            //Seite 7, Aufgabe 4
             //FileInfo fi_neu = new FileInfo("C:/001Projects/meine_datei.csv");
             //fi_neu.Delete();
             //Console.ReadKey();
 
-            //Seite2. Aufgabe 5
-            FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
+            //Seite 7, Aufgabe 5
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
 
-            Console.WriteLine("Gebe mir den Pfad an, wohin die Datei geschrieben werden soll!");
-            string path = Console.ReadLine();
-            path.Replace("\\", "\\\\");
-            DirectoryInfo di = new DirectoryInfo(path.Replace("\\", "\\\\"));
-            while (di.Exists == false)
+            //Console.WriteLine("Gebe mir den Pfad an, wohin die Datei geschrieben werden soll!");
+            //string path = Console.ReadLine();
+            //path.Replace("\\", "\\\\");
+            //DirectoryInfo di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+            //while (di.Exists == false)
+            //{
+            //    Console.WriteLine("Dein Pfad existiert nicht, gib mir einen neuen!");
+            //    path = Console.ReadLine();
+            //    di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+            //}
+            //Console.WriteLine("Wie soll die Datei heißen?");
+            //string fileName = Console.ReadLine();
+            //fi.CopyTo(path + fileName);
+            //Console.ReadKey();
+
+            //Seite 7, Aufgabe 6
+            FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
+            if (fi.Exists)
             {
-                Console.WriteLine("Dein Pfad existiert nicht, gib mir einen neuen!");
-                path = Console.ReadLine();
-                di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+                fi.CopyTo("C:/001Projects/meine_datei.csv");
             }
-            Console.WriteLine("Wie soll die Datei heißen?");
-            string fileName = Console.ReadLine();
-            fi.CopyTo(path + fileName);
-            Console.ReadKey();
 
 
         }
