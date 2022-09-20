cf4e343192a34ea11b8504571dbf09e333c7fa63
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 14:11:15 +0200

    //Seite7. Aufgabe 5

--- DataImport/DataImport/Program.cs
+++ DataImport/DataImport/Program.cs
@@ -16,6 +16,31 @@ namespace DataImport
             //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
             //fi.CopyTo("C:/001Projects/meine_datei.csv");
             //Console.ReadKey();
+
+            //Seite 2, Aufgabe 4
+            //FileInfo fi_neu = new FileInfo("C:/001Projects/meine_datei.csv");
+            //fi_neu.Delete();
+            //Console.ReadKey();
+
+            //Seite2. Aufgabe 5
+            FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
+
+            Console.WriteLine("Gebe mir den Pfad an, wohin die Datei geschrieben werden soll!");
+            string path = Console.ReadLine();
+            path.Replace("\\", "\\\\");
+            DirectoryInfo di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+            while (di.Exists == false)
+            {
+                Console.WriteLine("Dein Pfad existiert nicht, gib mir einen neuen!");
+                path = Console.ReadLine();
+                di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+            }
+            Console.WriteLine("Wie soll die Datei heißen?");
+            string fileName = Console.ReadLine();
+            fi.CopyTo(path + fileName);
+            Console.ReadKey();
+
+
         }
     }
 }
