bd6b9704b553b90d784748920f84bdca21cc3802
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 13:54:43 +0200

    no message

---
+++ DataImport/DataImport/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //004 Aufgaben Wiederholung Tag 1
+            //Seite 2, Aufgabe 2 & 3
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
+            //fi.CopyTo("C:/001Projects/meine_datei.csv");
+            //Console.ReadKey();
+        }
+    }
+}
